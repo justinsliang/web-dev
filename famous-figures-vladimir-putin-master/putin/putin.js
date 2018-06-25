0051f1726c71f54938c4789520d6526c5c6a1970
justinsliang <33615062+justinsliang@users.noreply.github.com>
2018-06-25 15:51:06 -0500

    Add files via upload

--- famous-figures-vladimir-putin-master/putin/putin.js
+++ famous-figures-vladimir-putin-master/putin/putin.js
@@ -1,4 +1,15 @@
-$(document).ready(function (){
-    $('h1').hover(h1OnMouseEnter, h1OnMouseOut);
-    $('ul li').hover(listHoverOn, listHoverOff);
+$(".btn").click(function () {
+    //api call here
+    var city = $(".city").val();
+    var uri = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",ru$units=si&appid=4e651d58543d2bad33b52678226ee6e6";
+    console.log (uri);
+    $.get(uri, onRes);
 });
+
+function onRes(response) {
+    console.log(response);
+    $(".country").html(response.sys.country);
+    $(".temp").html(response.main.temp);
+    $(".city-name").html(response.name);
+    $(".weather").html(response.weather[0].description);
+}
